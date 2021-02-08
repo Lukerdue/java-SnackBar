fc1adf29274f00de7290c2b8f57798949315041a
Jamie <jamielynjenks@gmail.com>
2021-02-08 13:28:17 -0700

    created Main

---
+++ src/snackbarApp/Main.java
@@ -0,0 +1,13 @@
+package snackbarApp;
+
+public class Main
+{
+    private static void workWithData()
+    {
+        System.out.println("Welcome to Lambda Snackbar!");
+    }
+    public static void main(String[] args)
+    {
+        workWithData();
+    }
+}
